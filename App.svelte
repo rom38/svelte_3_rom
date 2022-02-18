9ee478e5f4649abc1b4ea77f7f64eb7ac802e478
rom38 <rom38@users.noreply.github.com>
2022-02-18 08:44:38 +0800

    Initial commit

---
+++ App.svelte
@@ -0,0 +1,17 @@
+<script>
+  import Button from "./Button.svelte";
+  import PouchDB from 'pouchdb-browser';
+</script>
+
+<style>
+  main {
+    font-family: sans-serif;
+    text-align: center;
+  }
+</style>
+
+<main>
+	<h1>Hello CodeSandbox</h1>
+	<h2>Start editing to see some magic happen!</h2>
+	<Button />
+</main>
